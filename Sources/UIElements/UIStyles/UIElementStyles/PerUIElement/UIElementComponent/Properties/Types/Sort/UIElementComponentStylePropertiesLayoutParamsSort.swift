75cf86bb5c2126e7a68883b38f7fa44028e5b9e5
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2021-08-23 15:02:56 +0200

    Add sorts for look & layout params

---
+++ Sources/UIElements/UIStyles/UIElementStyles/PerUIElement/UIElementComponent/Properties/Types/Sort/UIElementComponentStylePropertiesLayoutParamsSort.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Łukasz Osiennik on 23/08/2021.
+//  Copyright © 2021 open plainness (https://www.openplainness.com). All rights reserved.
+//
+
+public enum UIElementComponentStylePropertiesLayoutParamsSort<UIElementComponentStylePropertiesLayoutParamsType: UIElementComponentStylePropertiesLayoutParams> {
+    
+    case os
+    case system(_ lookParams: UIElementComponentStylePropertiesLayoutParamsType)
+}
---
+++ Sources/UIElements/UIStyles/UIElementStyles/PerUIElement/UIElementComponent/Properties/Types/Sort/UIElementComponentStylePropertiesLookParamsSort.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Łukasz Osiennik on 23/08/2021.
+//  Copyright © 2021 open plainness (https://www.openplainness.com). All rights reserved.
+//
+
+public enum UIElementComponentStylePropertiesLookParamsSort<UIElementComponentStylePropertiesLookParamsType: UIElementComponentStylePropertiesLookParams> {
+    
+    case os
+    case system(_ lookParams: UIElementComponentStylePropertiesLookParamsType)
+}
