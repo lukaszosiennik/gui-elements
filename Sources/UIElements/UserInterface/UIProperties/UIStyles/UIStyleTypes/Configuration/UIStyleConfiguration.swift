843d19507a6b9c6e6ed43bbb1927f8f8066fbc3c
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2021-08-30 13:48:01 +0200

    Re-organize code

--- Sources/UIElements/UserInterface/UIProperties/UIStyles/UIStyleTypes/Configuration/UIStyleConfiguration.swift
+++ Sources/UIElements/UserInterface/UIProperties/UIStyles/UIStyleTypes/Configuration/UIStyleConfiguration.swift
@@ -3,11 +3,6 @@
 //  Copyright © 2020 open plainness (https://www.openplainness.com). All rights reserved.
 //
 
-public protocol UIStyleConfigurationInterface {
-    
-    static var current: UIStyleType { get }
-}
-
 public final class UIStyleConfiguration: UIStyleConfigurationInterface {
     
     public static var current: UIStyleType = .os
---
+++ Sources/UIElements/UserInterface/UIProperties/UIStyles/UIStyleTypes/Configuration/UIStyleConfigurationInterface.swift
@@ -0,0 +1,9 @@
+//
+//  Created by Łukasz Osiennik on 30/08/2021.
+//  Copyright © 2021 open plainness (https://www.openplainness.com). All rights reserved.
+//
+
+public protocol UIStyleConfigurationInterface {
+    
+    static var current: UIStyleType { get }
+}
