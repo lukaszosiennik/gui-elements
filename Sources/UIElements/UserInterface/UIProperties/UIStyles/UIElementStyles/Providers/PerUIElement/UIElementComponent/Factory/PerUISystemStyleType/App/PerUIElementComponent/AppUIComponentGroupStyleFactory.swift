ac77990e290e8d91d7c80dbbb7d618e4280d80ab
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2021-09-01 16:00:31 +0200

    Develop code

--- Sources/UIElements/UserInterface/UIProperties/UIStyles/UIElementStyles/Providers/PerUIElement/UIElementComponent/Factory/PerUISystemStyleType/App/PerUIElementComponent/AppUIComponentGroupStyleFactory.swift
+++ Sources/UIElements/UserInterface/UIProperties/UIStyles/UIElementStyles/Providers/PerUIElement/UIElementComponent/Factory/PerUISystemStyleType/App/PerUIElementComponent/AppUIComponentGroupStyleFactory.swift
@@ -8,8 +8,64 @@ public final class AppUIComponentGroupStyleFactory:
     UIComponentGroupStyleFactoryInterface {
     
     public func card() -> UIElementComponentStyle<CardUIComponentGroupStyleProperties> {
-        return .default(
-            styleType: styleType
+        return .init(
+            type: styleType,
+            properties: .init(
+                lookType: .system(.init(
+                    whole: .init(
+                        border: .init(
+                            corners: .rounded,
+                            width: 1,
+                            color: .init(hex: "#167649ff") ?? .black
+                        ),
+                        separator: .init(
+                            width: 1,
+                            color: .init(hex: "#157347ff") ?? .blue
+                        )
+                    ),
+                    title: .init(
+                        backgroundColor: .init(hex: "#188351ff") ?? .red,
+                        textColor: .init(hex: "#ffffffff") ?? .orange
+                    ),
+                    body: .init(
+                        backgroundColor: .init(hex: "#198754ff") ?? .green
+                    )
+                )),
+//                lookType: .system(.init(
+//                    whole: .init(
+//                        border: .init(
+//                            corners: .rounded,
+//                            width: 1,
+//                            color: .init(hex: "#dfa906ff") ?? .black
+//                        ),
+//                        separator: .init(
+//                            width: 1,
+//                            color: .init(hex: "#d8a406ff") ?? .blue
+//                        )
+//                    ),
+//                    title: .init(
+//                        backgroundColor: .init(hex: "#f7bb07ff") ?? .red,
+//                        textColor: .init(hex: "#212529ff") ?? .orange
+//                    ),
+//                    body: .init(
+//                        backgroundColor: .init(hex: "#ffc107ff") ?? .green
+//                    )
+//                )),
+                layoutParams: .init(
+                    title: .init(
+                        leftMargin: 16,
+                        rightMargin: 16,
+                        topMargin: 12,
+                        bottomMargin: 12
+                    ),
+                    body: .init(
+                        leftMargin: 16,
+                        rightMargin: 16,
+                        topMargin: 16,
+                        bottomMargin: 16
+                    )
+                )
+            )
         )
     }
     
