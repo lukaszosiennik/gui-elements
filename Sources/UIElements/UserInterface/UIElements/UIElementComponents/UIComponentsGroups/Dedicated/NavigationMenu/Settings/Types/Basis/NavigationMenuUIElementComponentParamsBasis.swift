145a59b1b3f1b68497e5f16c24e82c0d70480d7f
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2021-09-16 11:13:04 +0200

    develop code

--- Sources/UIElements/UserInterface/UIElements/UIElementComponents/UIComponentsGroups/Dedicated/NavigationMenu/Settings/Types/Basis/NavigationMenuUIElementComponentParamsBasis.swift
+++ Sources/UIElements/UserInterface/UIElements/UIElementComponents/UIComponentsGroups/Dedicated/NavigationMenu/Settings/Types/Basis/NavigationMenuUIElementComponentParamsBasis.swift
@@ -3,7 +3,7 @@
 //  Copyright © 2021 open plainness (https://www.openplainness.com). All rights reserved.
 //
 
-public class NavigationMenuUIElementComponentParamsBasis<
+open class NavigationMenuUIElementComponentParamsBasis<
     OptionKey:
         InputUIElementComponentActionKeyInterface,
     OptionValue:
