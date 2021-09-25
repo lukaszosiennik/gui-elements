dcd5fdbe1c395f5f2e1d129fc42d068e49b9415c
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2021-09-25 19:47:52 +0200

    develop code

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5
 
 import PackageDescription
 
@@ -9,7 +8,6 @@ let package = Package(
         .iOS(.v14),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GUIElements",
             targets: [
@@ -18,19 +16,18 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(
             url: "git@bitbucket.org:openplainness/commons.git",
             .branch("develop")
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GUIElements",
             dependencies: [
-                .byName(name: "commons"),
+                .byName(
+                    name: "commons"
+                ),
             ]
         ),
     ]
