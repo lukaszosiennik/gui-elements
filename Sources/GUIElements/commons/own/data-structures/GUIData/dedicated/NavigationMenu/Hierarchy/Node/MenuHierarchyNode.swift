b083720565448030848b3c9802c07c8f81672ca0
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2023-10-20 12:31:40 +0200

    develop code

---
+++ Sources/GUIElements/commons/own/data-structures/GUIData/dedicated/NavigationMenu/Hierarchy/Node/MenuHierarchyNode.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Łukasz Osiennik on 20/10/2023.
+//  Copyright © 2023 openplainness (https://www.openplainness.com). All rights reserved.
+//
+
+import commons
+
+typealias MenuHierarchyNode =
+    HierarchyNode<
+        MenuHierarchyNodeValue,
+        MenuHierarchyNodeChildrenLeafAction
+    >
---
+++ Sources/GUIElements/commons/own/data-structures/GUIData/dedicated/NavigationMenu/NavigationMenuHierarchyProvider.swift
@@ -0,0 +1,108 @@
+//
+//  Created by Łukasz Osiennik on 20/10/2023.
+//  Copyright © 2023 openplainness (https://www.openplainness.com). All rights reserved.
+//
+
+import commons
+
+final class NavigationMenuHierarchyProvider {
+    
+    typealias NavigationMenuHierarchyActions =
+        NavigationMenuGUIDataActions<
+            MenuHierarchyNodeChildrenLeafAction
+        >
+        
+    enum ProviderError:
+        Error {
+        
+        case nestedError(_ error: Error)
+    }
+    
+    private let decoder: NavigationMenuGUIDataDecoder
+    
+    init(
+        decoder: NavigationMenuGUIDataDecoder
+    ) {
+        self.decoder = decoder
+    }
+    
+    func rootNode(
+        styleType: GUIStyleType,
+        actions: NavigationMenuHierarchyActions
+    ) throws -> MenuHierarchyNode {
+        do {
+            return try node(
+                from: try decoder.decode(),
+                with: actions
+            )
+        } catch {
+            throw ProviderError.nestedError(
+                error
+            )
+        }
+    }
+    
+    private func node(
+        from menu: NavigationMenuGUIData.Menu,
+        with actions: NavigationMenuHierarchyActions
+    ) throws -> MenuHierarchyNode {
+        return .init(
+            value: .init(
+                strings: .init(
+                    title: menu.title
+                )
+            ),
+            childrenValue: .branch(
+                children: try children(
+                    from: menu.options,
+                    with: actions
+                )
+            )
+        )
+    }
+    
+    private func children(
+        from options: [NavigationMenuGUIData.Menu.Option],
+        with actions: NavigationMenuHierarchyActions
+    ) throws -> [MenuHierarchyNode] {
+        var children: [MenuHierarchyNode] = []
+        
+        try options.forEach { option in
+            children.append(.init(
+                value: .init(
+                    strings: .init(
+                        title: option.name
+                    )
+                ),
+                childrenValue: try childrenValue(
+                    from: option.kind,
+                    with: actions
+                )
+            ))
+        }
+        
+        return children
+    }
+    
+    private func childrenValue(
+        from optionKind: NavigationMenuGUIData.Menu.Option.Kind,
+        with actions: NavigationMenuHierarchyActions
+    ) throws -> HierarchyNodeChildrenValueUnion<
+        MenuHierarchyNode,
+        MenuHierarchyNodeChildrenLeafAction
+    > {
+        switch optionKind {
+        case .submenu(let menu):
+            return .branch(
+                children: try children(
+                    from: menu.options,
+                    with: actions
+                )
+            )
+        case .option(let actionId):
+            return .leaf(
+                action: try actions[actionId] ?? actions.unlinked
+            )
+        }
+    }
+}
