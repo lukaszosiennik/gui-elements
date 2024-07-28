60fbe162bc1c555dd369a6c029a8b86bf93fefcd
Łukasz Osiennik <lukasz.osiennik@gmail.com>
2024-07-28 14:32:18 +0200

    Clean-up code

--- Package.swift
+++ Package.swift
@@ -17,8 +17,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "commons",
-            dependencies: []
+            name: "commons"
         ),
         .target(
             name: "GUIElements",
